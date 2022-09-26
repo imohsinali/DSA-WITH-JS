091eff6a0ab998f09e8e10dc285da498d6284330
imohsinali <mohsinbalty@gmail.com>
2022-09-26 22:30:52 +0500

    Create recursion.js

---
+++ Recursion/recursion.js
@@ -0,0 +1,21 @@
+const app = (x) => {
+  // console.warn(x);
+
+  if (x < 10) {
+    app(1 + x);
+    // console.warn(x);
+  }
+};
+
+app(0);
+
+//5! 5 *4 *3 *2 *1
+
+const fun = (n) => {
+  if (n === 0) {
+    return 1;
+  }
+  return fun(n - 1) * n;
+};
+
+console.warn(fun(5));
