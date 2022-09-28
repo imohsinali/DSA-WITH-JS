d551f0d98f8f1ac003d0101e31c5e6b334ca70b0
imohsinali <mohsinbalty@gmail.com>
2022-09-28 22:28:24 +0500

    Update indirectrecurssion.js

--- Recursion/indirectrecurssion.js
+++ Recursion/indirectrecurssion.js
@@ -45,3 +45,38 @@ function topfun() {
 }
 
 topfun();
+
+//Head and Tail Recrucsion
+
+function text(x) {
+  console.warn(x); //head recursion 5 4 3 2 1 0
+  if (x > 0) {
+    test(x - 1);
+  }
+  // console.warn(x) //tail recursion 0 1 2 3 4 5
+}
+
+test(5);
+
+//recursion array with reverse
+
+let data = [3, 20, 9, 0];
+let temp;
+
+function reverseArray(data, start, end) {
+  console.warn(data);
+
+  if (start <= end) {
+    temp = data[start];
+    data[start] = data[end];
+    data[end] = temp;
+    reverseArray(data, start + 1, end - 1);
+  }
+}
+
+reverseArray(data, 0, data.length - 1);
+
+let data2 = [10, 20, 0, 15, 1];
+data2.reverse();
+// data2.sort((a, b) => b - a);
+console.warn(data2);
